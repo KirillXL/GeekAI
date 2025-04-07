f8815c8106901d78703f72a0429ef93108dd55db
KirillXL <kirillhoroshev.04.83@mail.ru>
2025-04-07 22:50:05 +0700

    Added interface element

--- app.py
+++ app.py
@@ -14,17 +14,6 @@ def chat():
 def admin():
     return render_template('admin.html')
 
-@app.route('/chat-content')
-def chat_content():
-    return render_template('chat-content.html')
-
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
-
-@app.route('/settings')
-def settings():
-    return render_template('settings.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
